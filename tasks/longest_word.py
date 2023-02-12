6f724a6e78f76cdbcd16a57dee8b01e45262e6f0
Igor <46938139+PlumNoseBear@users.noreply.github.com>
2023-02-12 20:46:38 +0000

    longest word

--- tasks/longest_word.py
+++ tasks/longest_word.py
@@ -3,12 +3,13 @@ def find_shortest_longest_word(text):
     В переданном тексте вернуть слово имеющее наименьшую и наибольшую длину.
     Если такого слова нет - вернуть None
     """
-    text=text.replace('\t',' ')
-    text=text.replace('\n',' ')
-    text =''.join(filter(str.isprintable, text))
+    text = text.replace('\t',' ')
+    text = text.replace('\n',' ')
+    text = ''.join(filter(str.isprintable, text))
     if len(text.replace(' ', '')) == 0:
-      maxword, minword = None, None 
+      maxword, minword = None, None
     else:
-      maxword =max(text.split(), key=len)
-      minword =min(text.split(), key=len)
+      maxword = max(text.split(), key=len)
+      minword = min(text.split(), key=len)
     return minword, maxword
+
