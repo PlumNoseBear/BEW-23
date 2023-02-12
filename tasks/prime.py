547cffe88e0927abfb782368c3f564deea1856f6
Igor <46938139+PlumNoseBear@users.noreply.github.com>
2023-02-13 00:27:28 +0500

    исправлено для 1 и 0

--- tasks/prime.py
+++ tasks/prime.py
@@ -2,9 +2,9 @@ def is_prime(number: int) -> bool:
     """
     Функция должна вернуть True если число является простым, иначе - False
     """
-    dig = 2
+    dig =2
     while dig**2 <= number and number % dig != 0:
         dig += 1
-    return dig**2 > number
+    return dig**2 > number if number >=2 else False
 
 
