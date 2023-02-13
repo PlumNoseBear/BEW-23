1fdcb8e7b8ca3068484a6a3006a4b3722c893891
Igor <46938139+PlumNoseBear@users.noreply.github.com>
2023-02-13 15:15:50 +0000

    все ж нормально

--- tasks/longest_word.py
+++ tasks/longest_word.py
@@ -22,3 +22,11 @@ def find_shortest_longest_word(text):
             max_word = word
 
     return (min_word, max_word)
+
+print(find_shortest_longest_word(''))
+print(find_shortest_longest_word('        '))
+print(find_shortest_longest_word('\n\n\t'))
+print(find_shortest_longest_word('hello\n\n\tsir'))
+print(find_shortest_longest_word('hello there, general kenobi'))
+print(find_shortest_longest_word('привет всем'))
+print(find_shortest_longest_word('привет       всем'))
