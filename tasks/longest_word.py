32b6f95f87157a244615c92391e9bbf2162fd000
Igor <46938139+PlumNoseBear@users.noreply.github.com>
2023-02-13 17:30:45 +0000

    Вариант  3 longesr word

--- tasks/longest_word.py
+++ tasks/longest_word.py
@@ -1,28 +1,14 @@
+import re
 def find_shortest_longest_word(text):
 
-    text = text.replace('\t',' ')
-    text = text.replace('\n',' ')
-    text = ''.join(filter(str.isprintable, text))
+    text = text.strip()
 
-    words = text.split()
-    if len(words) == 0:
-        return (None, None)
-    min_len = len(words[0])
-    max_len = len(words[0])
-    min_word = words[0]
-    max_word = words[0]
-
-    for word in words:
-        if len(word) < min_len:
-            min_len = len(word)
-            min_word = word
-
-        if len(word) > max_len:
-            max_len = len(word)
-            max_word = word
-
-    return (min_word, max_word)
-'''
+    if not text:
+        return None, None
+    text=re.sub(r'\s+', ' ', text)
+    words = re.split(r'\s+?', text)
+    words.sort(key=lambda w:len(w))
+    return words[0], words[1]
 print(find_shortest_longest_word(''))
 print(find_shortest_longest_word('        '))
 print(find_shortest_longest_word('\n\n\t'))
@@ -30,4 +16,3 @@ print(find_shortest_longest_word('hello\n\n\tsir'))
 print(find_shortest_longest_word('hello there, general kenobi'))
 print(find_shortest_longest_word('привет всем'))
 print(find_shortest_longest_word('привет       всем'))
-'''
